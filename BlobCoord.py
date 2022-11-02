62c23fa50e82bdc26ef2d6d3ac08fe70c6abf885
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-11-02 18:53:01 -0400

    More explanation

--- BlobCoord.py
+++ BlobCoord.py
@@ -26,8 +26,9 @@ import cv2
 import pandas
 import sklearn
 import sys
+from skimage.segmentation import slic 
 
-def pick_closest(data, centroid, dist):
+def calc_dist(data, centroid):
     centroid = np.array(centroid, dtype="float32")
     centroid = np.reshape(centroid, (1,3))
     data = data.astype("float32")
@@ -39,13 +40,21 @@ def pick_closest(data, centroid, dist):
     data = np.sum(data, axis=1)
     data = np.sqrt(data)
     print("data after sum:", data.shape)
+    return data
 
+def pick_within_thresh(data, centroid, dist):
+    all_dist = calc_dist(data, centroid)
     data = np.where(data < dist, 
                     np.ones(data.shape), 
                     np.zeros(data.shape))
-    print("final data:", data.shape)
-
+    data = np.uint8(data)
     return data
+
+def pick_closest(data, centroid):
+    all_dist = calc_dist(data, centroid)
+    print("all_dist:", all_dist.shape)
+    chosen_index = np.argmin(all_dist)
+    return chosen_index
 
 ###############################################################################
 # MAIN
@@ -98,15 +107,77 @@ def main():
             print("pixel_list:", pixel_list.shape)
 
             # BGR
-            chosen_ones = pick_closest(pixel_list, 
-                                        (0,0,255),
-                                        200)
+            chosen_index = pick_closest(pixel_list, 
+                                        (0,0,255))
 
-            chosen_ones = np.reshape(chosen_ones, frame.shape[0:2])
+            new_colors = np.zeros_like(pixel_list)
+            new_colors[chosen_index] = (255,255,255)
+            new_colors = np.reshape(new_colors, frame.shape)
+
+            # Call superpixel
+            segments = slic(frame, 
+                                n_segments=100,
+                                sigma=5,
+                                start_label=0,
+                                compactness=10)
+            cnt = len(np.unique(segments))
+            print("Number of superpixels:", cnt)
+            #print("Superpixels:", segments.shape, "\n", segments)
+
+            mask_image = np.where(segments == 50, 255, 0).astype("uint8")
+            mask_image = np.expand_dims(mask_image, axis=-1)
+            mean_color = cv2.mean(frame, mask=mask_image)[0:3]
             
+            '''
+            alt_color_list = [ (0,0,0), mean_color]
+            alt_color_list = np.array(alt_color_list, dtype="uint8")
+            mask_image = np.squeeze(mask_image, axis=-1)
+            mask_image = alt_color_list[mask_image]
+            print(mask_image.shape)
+            '''
+
+            k = 5 #10
+            _, bestLabels, centers = cv2.kmeans(
+                pixel_list.astype("float32"),
+                k,
+                None,
+                (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
+                10, 1.0), 10, cv2.KMEANS_RANDOM_CENTERS)
+
+            print("Best labels:", bestLabels.shape, "\n", bestLabels)
+            print("Kmeans centers:", centers.shape, "\n", centers)
+
+            chosen_index = pick_closest(centers, (0,0,255))
+            new_centers = np.zeros_like(centers)
+            new_centers[chosen_index] = (255,255,255)
+            new_centers = np.uint8(new_centers)
+            new_kmeans_colors = new_centers[bestLabels.flatten()]
+            new_kmeans_colors = np.reshape(new_kmeans_colors, frame.shape)
+
+            super_30 = np.where(segments == 30, 255, 0).astype("uint8")
+            super_60 = np.where(segments == 60, 255, 0).astype("uint8")
+            super_mask = super_30 + super_60
+
+            blobcnt, labels = cv2.connectedComponents(super_mask,
+                                                None,
+                                                8, cv2.CV_32S)
+
+            # First list y, second list x
+            coords = np.where(labels == 1)
+            print("coords:", coords)
+            ymin = np.amin(coords[0])
+            print("ymin:", ymin)
+
+            print("Blob cnt:", blobcnt)
+            print("labels:", labels.shape, "\n", labels)
+
+
             # Show the image
             cv2.imshow(windowName, frame)
-            cv2.imshow("CHOSEN", chosen_ones)
+            cv2.imshow("Superpixel 50", mask_image)
+            cv2.imshow("CHOSEN", new_kmeans_colors)
+            cv2.imshow("Supermask", super_mask)
+
 
             # Wait 30 milliseconds, and grab any key presses
             key = cv2.waitKey(30)
